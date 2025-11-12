12d6443955107cfe7162d9bbeea4340b824f73c0
Marina CASSAGNE <marina_cassagne@yahoo.fr>
2025-11-12 14:01:11 +0100

    feat: initialize npm project with express and nodemon dependencies
    - Created package.json for project metadata and dependencies
    - Added express as a dependency for server handling
    - Included nodemon as a devDependency for automatic server restarts
    
    feat: implement basic HTTP server
    
    - Created server.js to set up a local HTTP server
    - Handled GET requests to the root URL with a simple HTML response
    - Implemented 404 response for undefined routes

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+ if (req.url==="/"&& req.method==='GET') {
+    res.writeHead(200, { 'Content-Type': "text/html ; charset=utf-8"});
+    return res.end(
+        `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Hello</title>
+        </head>
+        <body>
+            
+        </body>
+        </html>`
+    );
+} 
+    res.writeHead(404);
+    return res.end("Not found"); //Not found login page
+});
+
+server.listen(3000);
